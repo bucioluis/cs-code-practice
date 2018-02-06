aa6aeddf0134df75b7af02921d17fab250783b22
Jose Luis Bucio <jlbucio4@gmail.com>
2018-02-05 22:43:07 -0600

    Update TextIO.java

--- Debug-Me/TextIO.java
+++ Debug-Me/TextIO.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: TextIO.java, CS125 Project: Challenge1-DebugMe, Version: 2015-02-04T09:52:42-0600.340822045
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
