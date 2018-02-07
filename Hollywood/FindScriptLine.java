0819e847ecb9220d2d9c6457594e59349f8813fe
Jose Luis Bucio <jlbucio4@gmail.com>
2018-02-06 18:06:49 -0600

    Update FindScriptLine.java

--- Hollywood/FindScriptLine.java
+++ Hollywood/FindScriptLine.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: FindScriptLine.java, CS125 Project: Challenge2-Hollywood, Version: 2015-02-09T07:33:26-0600.068983074
+
 /**
  * A program to search for specific lines and print their line number.
  * See FindScriptLine.txt for more details
