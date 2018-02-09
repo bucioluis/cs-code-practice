beab688f56b9d749b68fdd8b3b45e636dc87d40f
Jose Luis Bucio <jlbucio4@gmail.com>
2018-02-08 23:18:20 -0600

    Update CaesarCipher.java

--- TopSecret/CaesarCipher.java
+++ TopSecret/CaesarCipher.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: CaesarCipher.java, CS125 Project: Challenge3-TopSecret, Version: 2015-02-16T07:03:45-0600.471425819
+
 /**
  * A program to search for to encrypt and decrypt lines of text. See
  * CaesarCipher.txt
