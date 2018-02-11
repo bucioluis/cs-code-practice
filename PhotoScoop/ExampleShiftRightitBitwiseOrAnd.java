a9b0a971642be83aa175fb2241f972e3164ab686
Jose Luis Bucio <jlbucio4@gmail.com>
2018-02-10 23:47:07 -0600

    Update ExampleShiftRightitBitwiseOrAnd.java

--- PhotoScoop/ExampleShiftRightitBitwiseOrAnd.java
+++ PhotoScoop/ExampleShiftRightitBitwiseOrAnd.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: ExampleShiftRightitBitwiseOrAnd.java, CS125 Project: Challenge4-Photoscoop, Version: 2015-02-22T21:58:14-0600.814513453
+
 public class ExampleShiftRightitBitwiseOrAnd {
 
 	// Play with ExampleHexademical first!
