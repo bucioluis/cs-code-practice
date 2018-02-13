a1b0d918d4d65279f811fa98b0ab6c8effbea026
Jose Luis Bucio <jlbucio4@gmail.com>
2018-02-12 19:58:23 -0600

    Update PersonTest.java

--- Recursion-See/PersonTest.java
+++ Recursion-See/PersonTest.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: PersonTest.java, CS125 Project: Challenge6-RecursionSee, Version: 2015-03-30T08:07:53-0500.286760888
+
 import junit.framework.TestCase;
 /**
  * Test file. You do do not need to modify this file.
